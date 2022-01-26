0fd2812e54ca082271d333802240fcf3b60b9376
mahalakshmiyml <lakshmiyml@gmail.com>
2022-01-26 11:20:56 +0530

    contentuploaded

--- src/Components/WebsiteSolutions/ECommerce.js
+++ src/Components/WebsiteSolutions/ECommerce.js
@@ -1,5 +1,6 @@
 import React from "react";
-import TopHeaderPage from './../Includes/TopHeaderPage';
+import TopHeaderPage from "./../Includes/TopHeaderPage";
+import { Col, Container, Row } from "react-bootstrap";
 
 const ECommerce = () => {
   return (
@@ -13,6 +14,83 @@ const ECommerce = () => {
           link="/website-solutions"
           linkingPageName="Website Solutions"
         ></TopHeaderPage>
+      </section>
+      <section className="pt-3 pb-3 pt-md-5 pb-md-5">
+        <Container>
+          <Row>
+            <Col md={12}>
+              <p>
+                eCommerce websites are more than just a place to find
+                information. They are also a place to buy things. The purpose of
+                e-commerce websites is to allow consumers to submit their
+                billing and shipping addresses, as well as their chosen payment
+                methods, in a more hands-on manner to expedite commercial
+                transactions that involve data transfer online
+              </p>
+              <p>
+                The convenience of purchasing items and services from the
+                comfort of one's own home and expecting delivery in a short
+                period of time is made possible through this platform. Amazon,
+                Flipkart, Myntra, and so on are some of the most popular
+                e-commerce sites.
+              </p>
+              <p>
+                If you're selling a Rs. 5500 salwar kameez, an online course for
+                Rs. 21000, or a Rs. 400-rupee book, your customers should get an
+                e-commerce experience thhat is both simple and bold.
+              </p>
+              <h4>
+                Our eCommerce Web Design Agency Creates Online Stores That Sell
+              </h4>
+              <p>
+                Shanthaswebz is a digital marketing and web design agency, a
+                place where research-driven strategy meets innovative design to
+                generate income and achieve outcomes. When it comes to
+                partnerships, we aren't looking to just collaborate with
+                well-known companies; rather, we want to work with bold
+                individuals who want to set the bar high and help to reshape the
+                digital landscape of their respective industries.
+              </p>
+              <p>
+                Shanthaswebz recognises the necessity of employing the newest
+                technologies to increase profits. Our eCommerce solutions are
+                flexible and entirely customized. A unique eCommerce solution
+                that integrates with ERP systems, pulls data from many sources,
+                and matches your brand and eCommerce goals. We are the best
+                choice for your project since we have years of eCommerce
+                experience and design services.
+              </p>
+              <h4>How We Work as an eCommerce Web Design Company</h4>
+              <p>
+                When it comes to eCommerce web design and development, we at
+                eScale normally follow a certain procedure. In a nutshell, here
+                is how it works:
+              </p>
+              <h5>Initial professional consultation:</h5>
+              <p>Initially, we meet with a potential client to find out
+              exactly what they are looking for and whether or not we are a
+              suitable fit for each other (on both sides). As you can see, we
+              don't work with just anyone. We only partner with companies that
+              have a purposeful, positive influence or are making a difference
+              in some way.</p>
+              <h5>A blueprint should be drawn up:</h5>
+              <p>A nice fit is found, and our new customer sends us a list of websites they enjoy and want to use as inspiration for their new eCommerce business. Now that we have this information, we can start creating a blueprint for the website's appearance, feel, and organization.</p>
+              <h5>Make a prototype of your project:</h5>
+              <p>The next step is to create a wireframe, which is a visual representation of the website's structure and the content that will appear there.</p>
+              <h5>The process of conceptualizing and creating:</h5>
+              <p>Finally, there is the design and construction of an e-commerce website. Not only do we design for the desktop, but we also design for mobile phones. That way, your visitors won't need their reading glasses because your website's material is easy to read!</p>
+              <p>In addition, what you see in the design is what you receive in the final product. With many agencies, the final product is frequently of lower quality than the original design... but not with us.</p>
+              <h5>Keep testing, testing, testing:</h5>
+              <p>In order to verify that a website is ready to go live, we do comprehensive testing and quality assurance. Yes, you might categorize us as perfectionists...</p>
+
+              <h3 className="text-primary">Your online success depends on us</h3>
+              <p>The e-commerce industry is one of the most fiercely competitive in the world today. A professional agency is essential if you want to make your website stand out from the crowd.</p>
+              <p>Your ecommerce website requires the expertise of a firm that understands how to design category and product pages as well as landing pages, contest entries, form fields, contact pages, and all of the other necessary components of an ecommerce website.</p>
+              <p>You can be certain that you will lose important traffic and clients to your competition if you do not have one.</p>
+              <h6 className="text-primary">You don't want that to happen! Make a move now to contact us, Shanthaswebz, the best e-commerce web design company for your requirements, and let's begin working together!</h6>
+            </Col>
+          </Row>
+        </Container>
       </section>
     </main>
   );
--- src/Components/WebsiteSolutions/Shopify.js
+++ src/Components/WebsiteSolutions/Shopify.js
@@ -1,22 +1,98 @@
-import React from 'react'
-import TopHeaderPage from './../Includes/TopHeaderPage';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import TopHeaderPage from "./../Includes/TopHeaderPage";
 
 const Shopify = () => {
-    return (
-        
+  return (
     <main>
-    <section
-      className="bg-primary pt-3 pb-3 pt-md-5 pb-md-5"
-      id="page-top-header"
-    >
-      <TopHeaderPage
-        name="Shopify"
-        link="/website-solutions"
-        linkingPageName="Website Solutions"
-      ></TopHeaderPage>
-    </section>
-  </main>
-    )
-}
+      <section
+        className="bg-primary pt-3 pb-3 pt-md-5 pb-md-5"
+        id="page-top-header"
+      >
+        <TopHeaderPage
+          name="Shopify"
+          link="/website-solutions"
+          linkingPageName="Website Solutions"
+        ></TopHeaderPage>
+      </section>
+      <section className="pt-3 pb-3 pt-md-5 pb-md-5">
+        <Container>
+          <Row>
+            <Col md={12}>
+              <p>
+                In the world of ecommerce, Shopify is one of the most well-known
+                and well-respected solutions. There are multiple key features
+                and it is simple to use, while remaining adaptable. An expert
+                Shopify ecommerce development firm like Shanthaswebz can assist
+                you in making the most of this platform's features.
+              </p>
 
-export default Shopify
+              <h6>
+                As an online retailer, what are the advantages of using Shopify?
+              </h6>
+              <h2>Shopify.....</h2>
+              <p>
+                In order to get your online store up and running quickly,
+                Shopify offers a fully-hosted eCommerce platform. It's easy to
+                get up and running with more than 100 professional themes and a
+                number of handy plugins.{" "}
+              </p>
+              <p>
+                Dropshipping, fraud prevention, SEO optimization, social media
+                integration, and more are just a few of the eCommerce
+                capabilities that our Shopify developers can implement for your
+                business's online store.
+              </p>
+              <p>
+                It has more than 1.7 million merchants utilizing the platform,
+                making it a popular choice for enterprises across a wide range
+                of sectors. There are several reasons to use the platform. The
+                following are some of the platform's advantages:
+              </p>
+              <h5>Setup and usage are a breeze:</h5>
+              <p>
+                Users of Shopify will find it simple and easy to use. Because
+                it's a hosted platform, the process of setting it up is a lot
+                faster and easier. As a plus, it has a simple, drag-and-drop
+                interface and various pre-built themes to help you quickly
+                launch an online store.
+              </p>
+              <h5>An impressive array of built-in features:</h5>
+              <p>
+                Shopify has a wide range of tools and capabilities that are
+                crucial for running an e-commerce business. It consists of the
+                following elements:
+              </p>
+              <ul>
+                <li>Features that ensure safety</li>
+                <li>Intuitive navigation</li>
+                <li>Themes that are responsive</li>
+                <li>Various payment methods are available</li>
+              </ul>
+              <h3 className="text-primary">Shopify  solutions from Shanthaswebz</h3>
+              <p>Take your eCommerce business to the next level with feature-rich, responsive, cross-platform, and visually appealing online storefronts.</p>
+              <h5>Shopify Store Setup:</h5>
+              <p>Your business will benefit from our full range of Shopify website construction and configuration services, including theme installation, product information setup, picture uploads, marketing, chatbots, and more.</p>
+              <h5>Customization of the theme:</h5>
+              <p>PSD conversion, feature customization, layout design, and mobile-centric theme creation are all available as part of our results-driven Shopify theme development services.</p>
+              <h5>Integration of Third-Party Apps</h5>
+              <p>Third-party apps, such as social networking, payment gateways, and delivery methods, may be integrated into your Shopify eCommerce store to expand its usefulness.</p>
+              <p>At Shanthaswebz, we use Shopify's SEO capabilities and optimize the site architecture to assist you in directing more traffic and sales to your store.</p>
+              <h5>Maintenance and support:</h5>
+              <p>A comprehensive support service is provided to guarantee that your website is up to date with the newest technology and provides the best possible user experience for your visitors.</p>
+              <h5>Shopify migration:</h5>
+              <p>In addition to creating mobile-friendly custom Shopify themes, our Shopify app development firm builds highly customized cross-platform mobile applications for your eStore.</p>
+              <h5>POS Design for Shopify:</h5>
+              <p>With our Shopify POS development services, we help you build an omnichannel eCommerce business strategy, boost your user base, and ensure smooth transactions across numerous platforms.</p>
+              <h5 className="text-primary">Take advantage of our Shopify web development services with Shanthaswebz in order to safeguard your technological investments.</h5>
+              <p>Working with our top Shopify app developers will get you highly scalable solutions. Our Shopify development company offers quick time-to-market, round-the-clock customer service, and total customisation. Allow us to handle all of your Shopify eCommerce development needs, from upgrades and redesigns to a brand-new solution.</p>
+              <p>By creating a Shopify website, you can begin to reap the benefits of using Shopify to sell your products or services online. Reach us online or by phone at 123 456 789 to get the ball rolling on your project.</p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </main>
+  );
+};
+
+export default Shopify;
